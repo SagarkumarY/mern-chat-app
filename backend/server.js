d5a18c1c6976b02a21bd200e61a6e49dd9eced81
Sagar <sagaryadav47232@gmail.com>
2024-02-20 19:57:21 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,7 @@
 
 
 
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -53,15 +54,13 @@ import { app,server } from './socket/socket.js';
 // Set port from environment variable or default to 5000
 const PORT = process.env.PORT || 5000;
 
+
+const __dirname = path.resolve()
+
 // Middleware
 app.use(cors());
 app.use(express.json()); // Parse incoming JSON data
 app.use(cookieParser()); // Parse cookies
-
-// Default route
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
 
 // Authentication routes
 app.use('/api/auth', authRouter);
@@ -71,6 +70,15 @@ app.use('/api/message', messageRouter);
 
 // Users routes
 app.use('/api/users', usersRouter);
+
+
+// Serve static files from the frontend/dist directory
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+// For any other route, serve the index.html file
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 // Start the server
 server.listen(PORT, () => {
